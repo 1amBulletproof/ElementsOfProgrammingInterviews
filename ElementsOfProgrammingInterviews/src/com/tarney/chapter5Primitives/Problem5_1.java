5885738cb8fd6b6ca8df349ceb27004f5cc56d8d
1amBulletproof <brandon.tarney@gmail.com>
2016-05-27 20:50:26 -0400

    Added problem description

--- ElementsOfProgrammingInterviews/src/com/tarney/chapter5Primitives/Problem5_1.java
+++ ElementsOfProgrammingInterviews/src/com/tarney/chapter5Primitives/Problem5_1.java
@@ -1,5 +1,15 @@
 /*
  * Solution to problem 5.1 on page 43 of Elements of Programming Interviews
+ * <p>
+ * <em>Problem:</em> The parity of a binary word is 1 if the number of 1s in the word is odd; otherwise it is 0.
+ * For example, the parity of 1011 is 1, and the parity of 100001000 is 0. Parity checks are used to detect single bit errors in data storage and communication. 
+ * It is fairly straightforward to write code that computes the parity of a single 64-bit word. 
+ * <strong>How would you compute the parity of a very large number of 64 bit words?</strong>
+ * </p>
+ * 
+ * <p>
+ * <em>Hint: Use a lookup table, but don't use 2^64 entries!</em>
+ * </p>
  * 
  * @author Brandon Tarney
  * @version 1.0

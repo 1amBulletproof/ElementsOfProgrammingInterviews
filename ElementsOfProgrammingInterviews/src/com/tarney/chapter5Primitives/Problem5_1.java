475248130eff15a8c747a6f1a6aee2dc92e196c3
1amBulletproof <brandon.tarney@gmail.com>
2016-05-27 20:37:51 -0400

    start of problem 5.1

---
+++ ElementsOfProgrammingInterviews/src/com/tarney/chapter5Primitives/Problem5_1.java
@@ -0,0 +1,17 @@
+/*
+ * Solution to problem 5.1 on page 43 of Elements of Programming Interviews
+ * 
+ * @author Brandon Tarney
+ * @version 1.0
+ */
+
+package com.tarney.chapter5Primitives;
+
+public class Problem5_1 {
+	
+	
+	static public void main (String[] args) {
+		//TODO write main class
+	}
+
+}
